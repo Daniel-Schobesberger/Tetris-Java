aa4a80fecca86ac1ecf01f8c7748165ebcfde36e
Daniel-Schobesberger <43504653+Daniel-Schobesberger@users.noreply.github.com>
2020-04-29 15:16:28 +0200

    complete Tetris-Project Task 1

---
+++ Tetris/src/ui/DrawGame.java
@@ -0,0 +1,26 @@
+package ui;
+
+import infrastructure.Gui;
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JLabel;
+
+public class DrawGame extends JLabel{
+    
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        
+        g.setColor(Color.LIGHT_GRAY);
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 18; j++) {
+                g.drawRect(i*32, j*32, 32, 32);
+            }
+        }
+        
+        g.setColor(Color.BLACK);
+        g.drawRect(0, 0, Gui.width, Gui.height);
+        
+        repaint();
+    }
+}
